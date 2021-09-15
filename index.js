d7012b0bf398220f6cec7860926c0a4d9ba49fb4
Nguyễn Anh Tuấn <natuan.org@gmail.com>
2021-09-15 15:30:12 +0700

    release 1.0.5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // main index.js
 import { Platform, PickerIOS } from 'react-native'
 
-import WheelPicker from './WheelPicker'
+import WheelPicker from './src/WheelPicker'
 
 module.exports = Platform.OS === 'ios' ? PickerIOS : WheelPicker
